594b91cf5ee6627285434fc835909b844d111978
Sahil Saleem <70108175+sahilsaleem2907@users.noreply.github.com>
2022-08-20 02:14:44 +0530

    Time: 2 ms (93.39%), Space: 50 MB (39.27%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,37 +1,50 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         
-        int number1 = -1, number2 = -1, count1 = 0, count2 = 0, len = nums.length;
-    for (int i = 0; i < len; i++) {
-      if (nums[i] == number1)
-        count1++;
-      else if (nums[i] == number2)
-        count2++;
-      else if (count1 == 0) {
-        number1 = nums[i];
-        count1 = 1;
-      } else if (count2 == 0) {
-        number2 = nums[i];
-        count2 = 1;
-      } else {
-        count1--;
-        count2--;
-      }
-    }
-    ArrayList < Integer > ans = new ArrayList < Integer > ();
-    count1 = 0;
-    count2 = 0;
-    for (int i = 0; i < len; i++) {
-      if (nums[i] == number1)
-        count1++;
-      else if (nums[i] == number2)
-        count2++;
-    }
-    if (count1 > len / 3)
-      ans.add(number1);
-    if (count2 > len / 3)
-      ans.add(number2);
-    return ans;
+        int count1 = 0, count2= 0 , element1 = -1 , element2 = -1, n = nums.length;
+        
+        for(int i : nums){
+            
+            if(i == element1) ++count1;
+            
+            else if(i == element2) ++count2;
+            
+            else if(count1 == 0){
+                element1 = i;
+                count1 = 1;
+            }
+            
+            else if(count2 == 0){
+                element2 = i;
+                count2 = 1;
+            }else{
+                count1--;
+                count2--;
+            }
+            
+        }
+        
+        
+        List<Integer> res = new ArrayList<>();
+        
+        int cnt1 = 0 , cnt2 = 0;
+        
+        for(int k : nums){
+            
+            if(k == element1)
+                cnt1++;
+            else if(k == element2)
+                cnt2++;
+            
+        }
+        
+        if(cnt1 > n / 3)
+            res.add(element1);
+        if(cnt2 > n / 3)
+            res.add(element2);
+        
+        return res;
+        
         
     }
 }
