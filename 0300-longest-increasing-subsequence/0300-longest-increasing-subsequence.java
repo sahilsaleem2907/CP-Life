469da7069f8c562b97af41daeefea8d184252d3a
Sahil Saleem <70108175+sahilsaleem2907@users.noreply.github.com>
2022-11-05 13:51:29 +0530

    Time: 13 ms (81.64%), Space: 44.2 MB (65.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,31 +1,48 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        
-        int[][]dp = new int[n][n+1];
-        Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
-        
-        return helper(0,-1,n,nums,dp);
+    int[] tails = new int[nums.length];
+    int size = 0;
+    for (int x : nums) {
+        int i = 0, j = size;
+        while (i != j) {
+            int m = (i + j) / 2;
+            if (tails[m] < x)
+                i = m + 1;
+            else
+                j = m;
+        }
+        tails[i] = x;
+        if (i == size) ++size;
     }
+    return size;
+}
+//      public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+        
+//         int[][]dp = new int[n][n+1];
+//         Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
+        
+//         return helper(0,-1,n,nums,dp);
+//     }
     
     
-    public int helper(int index, int prev,int n, int[] nums,int[][] dp){
+//     public int helper(int index, int prev,int n, int[] nums,int[][] dp){
         
-         if(index == n) return 0 ;
+//          if(index == n) return 0 ;
          
-         if(dp[index][prev+1] != -1) return dp[index][prev+1];
+//          if(dp[index][prev+1] != -1) return dp[index][prev+1];
         
          
-         int length = 0 + helper(index+1,prev,n,nums,dp); //not taking the item
+//          int length = 0 + helper(index+1,prev,n,nums,dp); //not taking the item
         
-         if(prev == -1 || nums[index] > nums[prev]){
+//          if(prev == -1 || nums[index] > nums[prev]){
             
-            length = Math.max(length,1+helper(index+1,index,n,nums,dp));
+//             length = Math.max(length,1+helper(index+1,index,n,nums,dp));
             
-         }
+//          }
         
         
-         return dp[index][prev+1] = length;
+//          return dp[index][prev+1] = length;
         
-    }
+//     }
 }
