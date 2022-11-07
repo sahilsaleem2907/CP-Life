2babafb2d6bff9f0cea5acb7f754c0112c00ba57
Sahil Saleem <70108175+sahilsaleem2907@users.noreply.github.com>
2022-11-07 14:57:05 +0530

    Time: 13 ms (35.49%), Space: 44.6 MB (72.97%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -4,29 +4,28 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
         
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n+1][m+1];
         
-        Arrays.stream(dp).forEach(a -> Arrays.fill(a,-1));
+        for(int i = 0; i <= n ;i++) dp[i][0] = i;
+        for(int j = 0; j <= m ;j++) dp[0][j] = j;
         
-        return helper(n-1,m-1,word1,word2,dp);
+        for(int i = 1 ; i <= n ;i++){
+            for(int j = 1; j <= m; j++){
+        
+            if(word1.charAt(i-1) == word2.charAt(j-1)){
+            //matches
+                dp[i][j] = 0 + dp[i-1][j-1];
+            }else{
+            
+                dp[i][j] = 1 + Math.min(Math.min(dp[i][j-1],dp[i-1][j]),dp[i-1][j-1]);
+            
+            }
+                
+            }
+        }
+        
+        return dp[n][m];
         
     }
     
-    public int helper(int index1, int index2, String w1, String w2,int[][] dp){
-        
-        if(index1 < 0) return index2+1;
-        
-        if(index2 < 0) return index1+1;
-        
-        if(dp[index1][index2] != -1) return dp[index1][index2];
-        
-        
-        if(w1.charAt(index1) == w2.charAt(index2)){
-            //matches
-            return dp[index1][index2] = 0 + helper(index1 - 1, index2 -1 ,w1,w2,dp);
-        }
-        else return dp[index1][index2] = 1 + Math.min(Math.min(helper(index1, index2 -1,w1,w2,dp),helper(index1-1, index2,w1,w2,dp)),helper(index1-1, index2 -1 ,w1,w2,dp));
-        
-        
-    }
 }
