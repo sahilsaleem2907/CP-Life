293965c1fba68f97c93b75217c6b77a18e851b31
Sahil Saleem <70108175+sahilsaleem2907@users.noreply.github.com>
2022-11-07 14:57:40 +0530

    Time: 6 ms (93.32%), Space: 42.5 MB (85.73%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -28,4 +28,36 @@ class Solution {
         
     }
     
+    
+//     public int minDistance(String word1, String word2) {
+        
+//         int n = word1.length();
+//         int m = word2.length();
+        
+//         int[][] dp = new int[n][m];
+        
+//         Arrays.stream(dp).forEach(a -> Arrays.fill(a,-1));
+        
+//         return helper(n-1,m-1,word1,word2,dp);
+        
+//     }
+    
+//     public int helper(int index1, int index2, String w1, String w2,int[][] dp){
+        
+//         if(index1 < 0) return index2+1;
+        
+//         if(index2 < 0) return index1+1;
+        
+//         if(dp[index1][index2] != -1) return dp[index1][index2];
+        
+        
+//         if(w1.charAt(index1) == w2.charAt(index2)){
+//             //matches
+//             return dp[index1][index2] = 0 + helper(index1 - 1, index2 -1 ,w1,w2,dp);
+//         }
+//         else return dp[index1][index2] = 1 + Math.min(Math.min(helper(index1, index2 -1,w1,w2,dp),helper(index1-1, index2,w1,w2,dp)),helper(index1-1, index2 -1 ,w1,w2,dp));
+        
+        
+//     }
+    
 }
