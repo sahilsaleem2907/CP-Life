000b7dcc9b7fa33138161bd94d7e67bd60da481c
Sahil Saleem <70108175+sahilsaleem2907@users.noreply.github.com>
2022-11-11 00:12:37 +0530

    Time: 10 ms (66.92%), Space: 42.5 MB (83.44%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,36 +1,56 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
         
-        Set<String> set = new HashSet<>();
+        dp[0] = true; //base case
         
-        for(String s1 : wordDict){
+        for(int i = 1 ; i <= n ; i++){
             
-            set.add(s1);
-            
-        }
-        
-        return helper(s,set, new HashMap<String,Boolean>());
-        
-    }
-    
-    public boolean helper(String s, Set<String> set, Map<String,Boolean> map){
-        
-        if(s.length() == 0) return true;
-        
-        if(map.containsKey(s)) return map.get(s);
-        
-        for(int i = 1 ; i <= s.length() ; i++){
-            
-            if(set.contains(s.substring(0,i)) && helper(s.substring(i,s.length()),set,map)){
+            for(int j = 0 ; j < i; j++){
                 
-                map.put(s.substring(i,s.length()),true);
-                return true;
+                if(dp[j] && wordDict.contains(s.substring(j,i))) dp[i] = true;
+                
             }
             
         }
         
-        map.put(s,false);
-        return false;
-        
+        return dp[n];
     }
+    
+/* Recursion + Memoization */
+//     public boolean wordBreak(String s, List<String> wordDict) {
+        
+//         Set<String> set = new HashSet<>();
+        
+//         for(String s1 : wordDict){
+            
+//             set.add(s1);
+            
+//         }
+        
+//         return helper(s,set, new HashMap<String,Boolean>());
+        
+//     }
+    
+//     public boolean helper(String s, Set<String> set, Map<String,Boolean> map){
+        
+//         if(s.length() == 0) return true;
+        
+//         if(map.containsKey(s)) return map.get(s);
+        
+//         for(int i = 1 ; i <= s.length() ; i++){
+            
+//             if(set.contains(s.substring(0,i)) && helper(s.substring(i,s.length()),set,map)){
+                
+//                 map.put(s.substring(i,s.length()),true);
+//                 return true;
+//             }
+            
+//         }
+        
+//         map.put(s,false);
+//         return false;
+        
+//     }
 }
