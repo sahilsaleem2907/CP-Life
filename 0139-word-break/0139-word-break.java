6c91bb45964e922896187d85045f42cea0674b5d
Sahil Saleem <70108175+sahilsaleem2907@users.noreply.github.com>
2022-11-10 23:57:08 +0530

    Time: 16 ms (44.41%), Space: 47.6 MB (16.07%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+        Set<String> set = new HashSet<>();
+        
+        for(String s1 : wordDict){
+            
+            set.add(s1);
+            
+        }
+        
+        return helper(s,set, new HashMap<String,Boolean>());
+        
+    }
+    
+    public boolean helper(String s, Set<String> set, Map<String,Boolean> map){
+        
+        if(s.length() == 0) return true;
+        
+        if(map.containsKey(s)) return map.get(s);
+        
+        for(int i = 1 ; i <= s.length() ; i++){
+            
+            if(set.contains(s.substring(0,i)) && helper(s.substring(i,s.length()),set,map)){
+                
+                map.put(s.substring(i,s.length()),true);
+                return true;
+            }
+            
+        }
+        
+        map.put(s,false);
+        return false;
+        
+    }
+}
