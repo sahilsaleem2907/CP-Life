38c42518b355844d79fd9790d186ba2d08d90b2f
Sahil Saleem <70108175+sahilsaleem2907@users.noreply.github.com>
2022-11-23 05:23:57 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.java
+++ Topological sort - GFG/topological-sort.java
@@ -60,43 +60,83 @@ class Main {
 
 class Solution
 {
+    static void dfs(int node, int[] vis, ArrayList<ArrayList<Integer>> adj, Stack<Integer> st){
+        
+        
+      vis[node] = 1;
+      
+      for(int i : adj.get(node)){
+          
+          if(vis[i] == 0){
+              dfs(i,vis,adj,st);
+          }
+          
+      }
+      
+      st.push(node);
+        
+    }
     //Function to return list containing vertices in Topological order. 
     static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
     {
         // add your code here
-        int indegree[] = new int[V];
+        Stack<Integer> st = new Stack<>();
+        int vis[] = new int[V];
         int sort[] = new int[V];
         
-        for(int i = 0 ; i < indegree.length; i++){
-            for(int vertice : adj.get(i)){
-                indegree[vertice]++;
-            }
-        }
-        
-        Queue<Integer> q = new LinkedList<>();
-        
-        for(int i = 0 ; i < indegree.length; i++){
-            if(indegree[i] == 0) q.offer(i);
+        for(int i = 0 ; i < V; i++){
+            if(vis[i] == 0)
+                dfs(i,vis,adj,st);
         }
         
         int index = 0;
-        while(!q.isEmpty()){
-            
-            int node = q.poll();
-            sort[index++] = node;
-            
-            for(int child: adj.get(node)){
-                
-                indegree[child]--;
-                
-                if(indegree[child] == 0) q.offer(child);
-                
-            }
-            
+        while(!st.isEmpty()){
+            sort[index++] = st.pop();
         }
         
         return sort;
-        
-        
     }
+    
 }
+
+/* BFS */
+//  static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+//     {
+//         // add your code here
+        
+       
+//         int indegree[] = new int[V];
+//         int sort[] = new int[V];
+        
+//         for(int i = 0 ; i < indegree.length; i++){
+//             for(int vertice : adj.get(i)){
+//                 indegree[vertice]++;
+//             }
+//         }
+        
+//         Queue<Integer> q = new LinkedList<>();
+        
+//         for(int i = 0 ; i < indegree.length; i++){
+//             if(indegree[i] == 0) q.offer(i);
+//         }
+        
+//         int index = 0;
+//         while(!q.isEmpty()){
+            
+//             int node = q.poll();
+//             sort[index++] = node;
+            
+//             for(int child: adj.get(node)){
+                
+//                 indegree[child]--;
+                
+//                 if(indegree[child] == 0) q.offer(child);
+                
+//             }
+            
+//         }
+        
+//         return sort;
+        
+        
+//     }
