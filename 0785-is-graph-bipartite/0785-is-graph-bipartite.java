ba3b269e4b23ba5ec02fe35b4c67ec3c7c53defb
Sahil Saleem <70108175+sahilsaleem2907@users.noreply.github.com>
2022-11-23 23:09:20 +0530

    Time: 3 ms (42.26%), Space: 51.9 MB (81.25%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,32 +1,20 @@
 class Solution {
     
-    public boolean bfs(int start, int[] color, int[][] graph){
+    public boolean dfs(int start, int[] color, int[][] graph){
         
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-        color[start] = 0;
-        
-        while(!q.isEmpty()){
-            
-            int node = q.peek();
-            q.poll();
-            
-            for(int i : graph[node]){
-                if(color[i] == -1){
-                    color[i] = 1 - color[node];
-                    q.offer(i);
-                }else if(color[i] == color[node]){
+        for(int i : graph[start]){
+            if(color[i] == -1){
+                color[i] = 1-color[start];
+                if(!dfs(i,color,graph))
                     return false;
-                }
-            }
-            
+            }else if(color[i] == color[start])
+                return false;
         }
         
         return true;
     }
     
     public boolean isBipartite(int[][] graph) {
-        
         int V = graph.length;
         
         int color[] = new int[V];
@@ -35,11 +23,57 @@ class Solution {
         
         for(int i = 0 ; i < V; i++){
             if(color[i] == -1)
-                if(bfs(i,color,graph) == false)
+                if(dfs(i,color,graph) == false)
                     return false;
         }
         
         return true;
-        
     }
 }
+
+/* BFS */
+// class Solution {
+    
+//     public boolean bfs(int start, int[] color, int[][] graph){
+        
+//         Queue<Integer> q = new LinkedList<>();
+//         q.offer(start);
+//         color[start] = 0;
+        
+//         while(!q.isEmpty()){
+            
+//             int node = q.peek();
+//             q.poll();
+            
+//             for(int i : graph[node]){
+//                 if(color[i] == -1){
+//                     color[i] = 1 - color[node];
+//                     q.offer(i);
+//                 }else if(color[i] == color[node]){
+//                     return false;
+//                 }
+//             }
+            
+//         }
+        
+//         return true;
+//     }
+    
+//     public boolean isBipartite(int[][] graph) {
+        
+//         int V = graph.length;
+        
+//         int color[] = new int[V];
+        
+//         for(int i = 0;i<V;i++) color[i] = -1; 
+        
+//         for(int i = 0 ; i < V; i++){
+//             if(color[i] == -1)
+//                 if(bfs(i,color,graph) == false)
+//                     return false;
+//         }
+        
+//         return true;
+        
+//     }
+// }
