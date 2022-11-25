45e3eaa7fa6d002c61f22c7e97e7ba3763e15f56
Sahil Saleem <70108175+sahilsaleem2907@users.noreply.github.com>
2022-11-26 00:44:48 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.java
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.java
@@ -47,50 +47,153 @@ class DriverClass
 
 
 // User function Template for Java
-class Pair{
-    int node;
-    int distance;
+
+/* Kruskal Algorithm */
+class Node{
+    int u;
+    int v;
+    int weight;
     
-    Pair(int _node, int _distance){
-        node = _node;
-        distance =_distance;
+    Node(int _u,int _v,int _weight){
+        u = _u;
+        v = _v;
+        weight = _weight;
     }
     
+    int getU(){return u;}
+    int getV(){return v;}
+    int getWeight(){return weight;}
 }
+
+class SortComparator implements Comparator<Node>{
+    @Override
+    public int compare(Node node1, Node node2){
+        
+        if(node1.getWeight() < node2.getWeight())
+            return -1;
+        else if(node1.getWeight() > node2.getWeight())
+            return 1;
+            
+        return 0;
+    }
+}
+
 class Solution
 {
+    
+    static int findPar(int u, int[] parent){
+        if(u == parent[u]) return u;
+        
+        return parent[u] = findPar(parent[u],parent);
+    }
+    
+    static void union(int u, int v, int[] parent, int[] rank){
+        
+        u = findPar(u,parent);
+        v = findPar(v,parent);
+        
+        if(rank[u] < rank[v]){
+            parent[u] = v;
+        }else if (rank[u] > rank[v]){
+            parent[v] = u;
+        }else{
+            parent[v] = u;
+            rank[u]++;
+        }
+        
+    }
+    
     //Function to find sum of weights of edges of the Minimum Spanning Tree.
     static int spanningTree(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj) 
     {
         // Add your code here
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.distance - b.distance);
-        int[] visited = new int[V];
         
-        pq.offer(new Pair(0,0));
+        ArrayList<Node> adjUpdated = new ArrayList<Node>();
         
-        int sum = 0;
-        
-        while(!pq.isEmpty())
-        {
-            int node = pq.peek().node;
-            int wt = pq.peek().distance;
-            pq.poll();
-            
-            if(visited[node] == 1) continue;
-            
-            visited[node] = 1;
-            sum += wt;
-            
-            for(int i = 0; i< adj.get(node).size(); i++){
-                int adjNode = adj.get(node).get(i).get(0);
-                int edgeW = adj.get(node).get(i).get(1);
-                
-                if(visited[adjNode] == 0) pq.offer(new Pair(adjNode,edgeW));
+        for(int i = 0 ; i < V ; i++){
+            for(int j = 0 ; j < adj.get(i).size() ;  j++){
+                adjUpdated.add(new Node(i,adj.get(i).get(j).get(0),adj.get(i).get(j).get(1)));
             }
-            
         }
         
-        return sum;
+        Collections.sort(adjUpdated,new SortComparator());
+        int[] parent = new int[V];
+        int[] rank = new int[V];
+        
+        for(int i = 0 ; i  < V ; i++){
+            parent[i] = i;
+            rank[i] = 0;
+        }
+        
+        int cost = 0;
+        
+        ArrayList<Node> mst = new ArrayList<Node>();
+        
+        for(Node it : adjUpdated){
+            int u = it.getU();
+            int v = it.getV();
+            int wt = it.getWeight();
+            
+            if(findPar(u,parent) != findPar(v,parent)){
+                cost += wt;
+                mst.add(it);
+                union(u,v,parent,rank);
+            }
+        }
+        
+        
+        return cost;
+        
         
     }
 }
+
+/* Prims Algorithm */
+
+// class Pair{
+//     int node;
+//     int distance;
+    
+//     Pair(int _node, int _distance){
+//         node = _node;
+//         distance =_distance;
+//     }
+    
+// }
+// class Solution
+// {
+//     //Function to find sum of weights of edges of the Minimum Spanning Tree.
+//     static int spanningTree(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj) 
+//     {
+//         // Add your code here
+//         PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.distance - b.distance);
+//         int[] visited = new int[V];
+        
+//         pq.offer(new Pair(0,0));
+        
+//         int sum = 0;
+        
+//         while(!pq.isEmpty())
+//         {
+//             int node = pq.peek().node;
+//             int wt = pq.peek().distance;
+//             pq.poll();
+            
+//             if(visited[node] == 1) continue;
+            
+//             visited[node] = 1;
+//             sum += wt;
+            
+//             for(int i = 0; i< adj.get(node).size(); i++){
+//                 int adjNode = adj.get(node).get(i).get(0);
+//                 int edgeW = adj.get(node).get(i).get(1);
+                
+//                 if(visited[adjNode] == 0) pq.offer(new Pair(adjNode,edgeW));
+//             }
+            
+//         }
+        
+//         return sum;
+        
+//     }
+// }
