d6b47e47769a1a6f9545d553ddf7c91f312b82cf
Sahil Saleem <70108175+sahilsaleem2907@users.noreply.github.com>
2022-11-26 06:57:17 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.java
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.java
@@ -31,26 +31,26 @@ class Solution
 	{  
 	    //code here.
         int i, j, max = 0;
-        int msis[] = new int[n];
+        int dp[] = new int[n];
  
-        /* Initialize msis values
+        /* Initialize dp values
            for all indexes */
         for (i = 0; i < n; i++)
-            msis[i] = arr[i];
+            dp[i] = arr[i];
  
         /* Compute maximum sum values
            in bottom up manner */
         for (i = 1; i < n; i++)
             for (j = 0; j < i; j++)
                 if (arr[i] > arr[j] &&
-                    msis[i] < msis[j] + arr[i])
-                    msis[i] = msis[j] + arr[i];
+                    dp[i] < dp[j] + arr[i])
+                    dp[i] = dp[j] + arr[i];
  
         /* Pick maximum of all
-           msis values */
+           dp values */
         for (i = 0; i < n; i++)
-            if (max < msis[i])
-                max = msis[i];
+            if (max < dp[i])
+                max = dp[i];
  
         return max;
 	}  
