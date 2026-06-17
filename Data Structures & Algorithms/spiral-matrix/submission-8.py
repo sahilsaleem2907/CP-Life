8747ff3e0c4b1c31dad89a0e8d56dd35792ad100
Sahil <70108175+sahilsaleem2907@users.noreply.github.com>
2026-06-17 14:39:14 +0530

    Add: spiral-matrix - submission-8

---
+++ Data Structures & Algorithms/spiral-matrix/submission-8.py
@@ -0,0 +1,29 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+
+        res = []
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        steps = [ n , m - 1]
+        r, c, d = 0, -1, 0
+
+        while steps[d & 1]:
+            for i in range(steps[d & 1]):
+
+                r += directions[d][0]
+                c += directions[d][1]
+
+                res.append(matrix[r][c])
+
+            steps[d & 1] -= 1
+            d += 1
+            d %= 4
+        
+        
+        return res
+
+        
+        
